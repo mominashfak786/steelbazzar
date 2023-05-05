643e987047241711cc964d00402a27dc708db137
mominashfak786 <ashfakmomin2001@gmail.com>
2023-05-05 19:19:18 +0530

    done

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ function App() {
       {loadingComplete && (
         <>
           <Login />
-          <Create />
         </>
       )}
     </div>
--- src/components/create.js
+++ src/components/create.js
@@ -1,33 +1,108 @@
-import React from 'react'
-import "../styles/create.css"
-const create = () => {
+import React, { useState } from "react";
+import "../styles/create.css";
+import axios from 'axios';
+
+const Create = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    businessName: '',
+    gstNo: '',
+    email: '',
+    mobileNo: '',
+    password: ''
+  });
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value
+    }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    axios.post('/create', formData)
+      .then(() => {
+        window.location.href = '/dashboard';
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  
   return (
-      <>
-       <div className="containercreate">
-  <form className="card" action="" method='post'>
-    <div className="singup">Sign Up</div>
-    <div className="inputBox1">
-      <input type="text" required="required" />
-      <span className="user">Name</span>
+    <>
+      <div className="containercreate">
+        <form className="card" onSubmit={handleSubmit}>
+          <div className="singup">Sign Up</div>
+          <div className="inputBox1">
+            <input
+              type="text"
+              required=""
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
+            <span className="user">Name</span>
+          </div>
+          <div className="inputBox">
+            <input
+              type="text"
+              required=""
+              name="businessName"
+              value={formData.businessName}
+              onChange={handleInputChange}
+            />
+            <span className="user">Business Name</span>
           </div>
           <div className="inputBox1">
-      <input type="text" required="required" />
-      <span className="user">Email</span>
-    </div>
-    <div className="inputBox">
-      <input type="text" required="required" />
-      <span>Mobile No</span>
-    </div>
-    <div className="inputBox">
-      <input type="password" required="required" />
-      <span>Password</span>
-    </div>
-    <button className="enter">Sign Up</button>
-  </form>
-</div>
+            <input
+              type="text"
+              required=""
+              name="gstNo"
+              value={formData.gstNo}
+              onChange={handleInputChange}
+            />
+            <span className="user">GST No</span>
+          </div>
+          <div className="inputBox1">
+            <input
+              type="text"
+              required=""
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+            />
+            <span className="user">Email</span>
+          </div>
+          <div className="inputBox">
+            <input
+              type="text"
+              required=""
+              name="mobileNo"
+              value={formData.mobileNo}
+              onChange={handleInputChange}
+            />
+            <span>Mobile No</span>
+          </div>
+          <div className="inputBox">
+            <input
+              type="password"
+              required=""
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+            />
+            <span>Password</span>
+          </div>
+          <button type="submit" className="enter">
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
 
-      </>
-  )
-}
-
-export default create
+export default Create;
